f89863d0edf475fe39342467f6edd888bea504c7
Heitor Chang <heitorchang@gmail.com>
2018-02-16 23:14:20 -0200

    sample config

---
+++ learning_sql/config_sample.py
@@ -0,0 +1,10 @@
+# connection settings
+
+config = {
+    'user': 'root',
+    'password': 'secret',
+    'host': '127.0.0.1',
+    'database': 'learningpy',
+    'raise_on_warnings': True,
+    'use_pure': False,
+}
