5df45b8035d3d44139b276697f87f4765fe4b41d
Heitor Chang <heitorchang@gmail.com>
2018-05-04 15:06:24 -0300

    linear time solution

--- intro_algorithms_cormen/divide_and_conquer/max_subarray.py
+++ intro_algorithms_cormen/divide_and_conquer/max_subarray.py
@@ -24,27 +24,56 @@ def find_max_crossing_subarray(A, low, mid, high):
     return (max_left, max_right, left_sum+right_sum)
 
 def find_maximum_subarray(A, low, high):
-    if high == low:
-        return (low, high, A[low])
+    if high == low:                              # 1
+        return (low, high, A[low])               # 2
     else:
-        mid = (low + high) // 2
-        left_low, left_high, left_sum = find_maximum_subarray(A, low, mid)
-        right_low, right_high, right_sum = find_maximum_subarray(A, mid+1, high)
-        cross_low, cross_high, cross_sum = find_max_crossing_subarray(A, low, mid, high)
-        if left_sum >= right_sum and left_sum >= cross_sum:
-            return (left_low, left_high, left_sum)
-        elif right_sum >= left_sum and right_sum >= cross_sum:
-            return (right_low, right_high, right_sum)
+        mid = (low + high) // 2                  # 3 
+        left_low, left_high, left_sum = find_maximum_subarray(A, low, mid)                # 4
+        right_low, right_high, right_sum = find_maximum_subarray(A, mid+1, high)          # 5
+        cross_low, cross_high, cross_sum = find_max_crossing_subarray(A, low, mid, high)  # 6
+        if left_sum >= right_sum and left_sum >= cross_sum:     # 7
+            return (left_low, left_high, left_sum)              # 8
+        elif right_sum >= left_sum and right_sum >= cross_sum:  # 9
+            return (right_low, right_high, right_sum)           # 10
         else:
-            return (cross_low, cross_high, cross_sum)
+            return (cross_low, cross_high, cross_sum)           # 11
 
 def main(A):
     return find_maximum_subarray(A, 0, len(A)-1)
+
+
+def max_subarray_415_fail(A):
+    # p. 75 ex. 4.1-5
+    """Starting on the left, progress to the right"""
+    max_left = max_right = 0
+    max_sum = float('-inf')
+    for ptr in range(1, len(A)):
+        sum_add_right = sum(A[max_left:ptr])
+        print(max_left, ptr, sum_add_right)
+    return (max_left, max_right, max_sum)
+    
+
+def max_subarray_rutgers(A):
+    max_sum = float('-inf')
+    low = high = 0
+    running_sum = 0
+    running_low = 1
+    for i in range(len(A)):
+        running_sum += A[i]
+        if running_sum > max_sum:
+            low = running_low
+            high = i
+            max_sum = running_sum
+        if running_sum < 0:
+            running_sum = 0
+            running_low = i + 1
+    return (low, high, max_sum)
     
 def test():
     A = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
     B = [0,1,-5,3,4,-2,-1,7,-9]
     C = [-1,-1,-1,3,-1,-1,-1]
+    D = [-3,-1,-2]
     
     testeql(find_max_crossing_subarray(B, 0, 3, 8), (3,7,11))
 
@@ -52,3 +81,10 @@ def test():
     testeql(find_max_crossing_subarray(C, 0, 3, 6), (3,4,2))
 
     testeql(main(A), (7, 10, 18+20-7+12))
+
+    testeql(main(D), (1, 1, -1))
+
+    # testeql(max_subarray_415(A), (7, 10, 43))
+
+    testeql(max_subarray_rutgers(A), (7, 10, 43))
+
