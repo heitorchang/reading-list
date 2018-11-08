118345cb7fa0054119ca92e5fa4b416f4eb7601d
Heitor Chang <heitorchang@gmail.com>
2018-11-08 12:20:20 -0200

    correct spelling

--- interactive-python-ds/sep2018/trees/testbinheap.py
+++ interactive-python-ds/sep2018/trees/testbinheap.py
@@ -2,7 +2,7 @@ import os
 
 os.chdir("c:\\Users\\Heitor\\Desktop\\emacs-24.3\\bin\\reading-list\\interactive-py\\trees\\")
 
-from binaryhelp import BinaryHeap
+from binaryheap import BinaryHeap
 
 def test():
     pass
