13d08da98431bca0866a2b3622c1c1cf31c59c8e
Heitor Chang <heitorchang@gmail.com>
2019-04-26 14:03:23 -0300

    up to travel 4

---
+++ duolingo_chinese/count_chars.py
@@ -0,0 +1,25 @@
+# Load vocab.txt and tally how many different characters are there.
+# 713 characters up to House (75)
+
+import os
+
+DIRECTORY = "c:/Users/Heitor/Desktop/code/reading-list/duolingo_chinese/"
+
+INFILE = "vocab.txt"
+
+
+os.chdir(DIRECTORY)
+
+s = set()
+
+with open(INFILE, encoding="utf-8") as f:
+    for line in f:
+        line = line.strip()
+        if len(line) == 0 or line[0].isdigit():
+            continue
+        for c in line:
+            s.add(c)
+
+print(s)
+
+print(len(s))
