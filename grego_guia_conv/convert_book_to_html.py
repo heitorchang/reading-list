537ba6b48e21de077482e74e384bdb99c22a7dfb
Heitor Chang <heitorchang@gmail.com>
2020-03-11 11:05:45 -0300

    middle medico

---
+++ grego_guia_conv/convert_book_to_html.py
@@ -0,0 +1,5 @@
+# idea: check code point of first character,
+# if it's in the range of greek, apply a CSS class
+
+# if first word is number and first char of second word
+# is A-Z, then make it a header
