9f851b5ed27cb8e19a1db285674adf6a6ec5d86f
Heitor Chang <heitorchang@gmail.com>
2020-03-11 15:54:06 -0300

    end copying book

--- grego_guia_conv/convert_book_to_html.py
+++ grego_guia_conv/convert_book_to_html.py
@@ -1,5 +1,11 @@
+# For part_content.txt
+#
 # idea: check code point of first character,
 # if it's in the range of greek, apply a CSS class
 
 # if first word is number and first char of second word
 # is A-Z, then make it a header
+
+# For part_appendix.txt
+#
+# create headers only
