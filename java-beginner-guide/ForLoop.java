a19974876ccc8a06163aabc1d2664365128db146
Heitor Chang <heitorchang@gmail.com>
2020-04-21 16:17:23 -0300

    begin ch 2

---
+++ java-beginner-guide/ForLoop.java
@@ -0,0 +1,13 @@
+class ForLoop {
+    public static void main(String args[]) {
+        int ct;
+        ct = 0;
+
+        for (; ct <= 10; ct++) {
+            System.out.println(ct);
+        }
+
+        System.out.println("after loop");
+        System.out.println(ct);
+    }
+}
---
+++ java-beginner-guide/GalToLit.java
@@ -0,0 +1,11 @@
+class GalToLit {
+    public static void main(String args[]) {
+        double gallons, liters;
+
+        gallons = 10;
+
+        liters = gallons * 3.785;
+
+        System.out.println(gallons + " gallons is " + liters + " liters");
+    }
+}
