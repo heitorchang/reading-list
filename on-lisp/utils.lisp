88a381a66396e8bba7323ef8aba04bc0166194a3
Heitor Chang <heitor.chang@tempook.com>
2021-09-04 20:18:35 -0300

    on lisp

---
+++ on-lisp/utils.lisp
@@ -0,0 +1,40 @@
+;;; Fig. 4.1
+
+(defun last1 (lst)
+  (car (last lst)))
+
+(defun single (lst)
+  (and (consp lst) (not (cdr lst))))
+
+(defun append1 (lst obj)
+  (append lst (list obj)))
+
+(defun conc1 (lst obj)
+  (nconc lst (list obj)))
+
+(defun mklist (obj)
+  (if (listp obj) obj (list obj)))
+
+
+;;; Fig. 4.2
+
+(defun longer (x y)
+  (labels ((compare (x y)
+             (and (consp x)
+                  (or (null y)
+                      (compare (cdr x) (cdr y))))))
+    (if (and (listp x) (listp y))
+        (compare x y)
+        (> (length x) (length y)))))
+
+(defun group (source n)
+  (if (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+             (let ((rest (nthcdr n source)))
+               (if (consp rest)
+                   (rec rest (cons (subseq source 0 n) acc))
+                   (nreverse (cons source acc))))))
+    (if source (rec source nil) nil)))
+
+
+;;; Fig. 4.3
