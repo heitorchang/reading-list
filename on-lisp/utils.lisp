d61c0e4fc2312be14ea85a1bacc389f851f4dbec
Heitor Chang <heitor.chang@tempook.com>
2021-09-05 10:35:49 -0300

    utils

--- on-lisp/utils.lisp
+++ on-lisp/utils.lisp
@@ -27,7 +27,24 @@
         (compare x y)
         (> (length x) (length y)))))
 
+(defun filter (fn lst)
+  "Returns a list of whatever non-nil values produced by applying fn
+   to elements of lst.
+(filter #'(lambda (x) (if (numberp x) (1+ x))) '(a 1 2 b 3 c d 4 5))
+
+To get elements that pass a predicate, use remove-if-not
+"
+  (let ((acc nil))
+    (dolist (x lst)
+      (let ((val (funcall fn x)))
+        (if val (push val acc))))
+    (nreverse acc)))
+
 (defun group (source n)
+  "Group elements of source into sublists of length n.
+The remainder is put in a final sublist.
+(group '(a b c d e) 2)
+"
   (if (zerop n) (error "zero length"))
   (labels ((rec (source acc)
              (let ((rest (nthcdr n source)))
@@ -38,3 +55,161 @@
 
 
 ;;; Fig. 4.3
+
+(defun flatten (x)
+  (labels ((rec (x acc)
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(defun prune (test tree)
+  "Remove elements of tree where test is true.
+(prune #'evenp '(1 2 (3 (4 5) 6) 7 8 (9)))
+  "
+  (labels ((rec (tree acc)
+             (cond ((null tree) (nreverse acc))
+                   ((consp (car tree))
+                    (rec (cdr tree)
+                         (cons (rec (car tree) nil) acc)))
+                   (t (rec (cdr tree)
+                           (if (funcall test (car tree))
+                               acc
+                               (cons (car tree) acc)))))))
+    (rec tree nil)))
+
+;;; Fig. 4.4
+
+(defun find2 (fn lst)
+  "Return the first element where fn is true and the value returned by fn."
+  (if (null lst)
+      nil
+      (let ((val (funcall fn (car lst))))
+        (if val
+            (values (car lst) val)
+            (find2 fn (cdr lst))))))
+
+(defun before (x y lst &key (test #'eql))
+  "Check which of x and y occurs first in lst."
+  (and lst
+       (let ((first (car lst)))
+         (cond ((funcall test y first) nil)
+               ((funcall test x first) lst)
+               (t (before x y (cdr lst) :test test))))))
+
+(defun after (x y lst &key (test #'eql))
+  "Check which of x and y occurs later in lst. Both x and y must occur in the list."
+  (let ((rest (before y x lst :test test)))
+    (and rest (member x rest :test test))))
+
+(defun duplicate (obj lst &key (test #'eql))
+  (member obj (cdr (member obj lst :test test))
+          :test test))
+
+(defun split-if (fn lst)
+  (let ((acc nil))
+    (do ((src lst (cdr src)))
+        ((or (null src) (funcall fn (car src)))
+         (values (nreverse acc) src))
+      (push (car src) acc))))
+
+;;; Fig. 4.5
+
+(defun most (fn lst)
+  "Returns the element with the highest score (provided by fn)."
+  (if (null lst)
+      (values nil nil)
+      (let* ((wins (car lst))
+             (max (funcall fn wins)))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (when (> score max)
+              (setq wins obj
+                    max score))))
+        (values wins max))))
+
+(defun best (fn lst)
+  "Returns the element that beats all the others according to the predicate fn.
+fn must be a function of two arguments."
+  (if (null lst)
+      nil
+      (let ((wins (car lst)))
+        (dolist (obj (cdr lst))
+          (if (funcall fn obj wins)
+              (setq wins obj)))
+        wins)))
+
+(defun mostn (fn lst)
+  "Returns a list of all elements for which fn yields the highest score,
+and also the score itself."
+  (if (null lst)
+      (values nil nil)
+      (let ((result (list (car lst)))
+            (max (funcall fn (car lst))))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (cond ((> score max)
+                   (setq max score
+                         result (list obj)))
+                  ((= score max)
+                   (push obj result)))))
+        (values (nreverse result) max))))
+
+;; Fig. 4.6
+
+(defun map0-n (fn n)
+  (mapa-b fn 0 n))
+
+(defun map1-n (fn n)
+  (mapa-b fn 1 n))
+
+(defun mapa-b (fn a b &optional (step 1))
+  (do ((i a (+ i step))
+       (result nil))
+      ((> i b) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun map-> (fn start test-fn succ-fn)
+  (do ((i start (funcall succ-fn i))
+       (result nil))
+      ((funcall test-fn i) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun mappend (fn &rest lsts)
+  (apply #'append (apply #'mapcar fn lsts)))
+
+(defun mapcars (fn &rest lsts)
+  (let ((result nil))
+    (dolist (lst lsts)
+      (dolist (obj lst)
+        (push (funcall fn obj) result)))
+    (nreverse result)))
+
+(defun rmapcar (fn &rest args)
+  "A mapcar for trees (recursive mapcar)"
+  (if (some #'atom args)
+      (apply fn args)
+      (apply #'mapcar
+             #'(lambda (&rest args)
+                 (apply #'rmapcar fn args))
+             args)))
+
+;;; Fig. 4.7
+
+(defun readlist (&rest args)
+  (values (read-from-string
+           (concatenate 'string "("
+                        (apply #'read-line args)
+                        ")"))))
+
+(defun prompt (&rest args)
+  (apply #'format *query-io* args)
+  (read *query-io*))
+
+(defun break-loop (fn quit &rest args)
+  (format *query-io* "Entering break-loop.~%")
+  (loop
+        (let ((in (apply #'prompt args)))
+          (if (funcall quit in)
+              (return)
+              (format *query-io* "~a~%" (funcall fn in))))))
