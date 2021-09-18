b644a11b9764864ab0a06177c00e9f4a54828a75
Heitor Chang <heitor.chang@tempook.com>
2021-09-18 17:20:32 -0300

    middle of prolog

---
+++ paradigms-of-ai/prolog.lisp
@@ -0,0 +1,102 @@
+(defparameter fail nil "pat-match failure")
+
+(defparameter no-bindings '((t . t))
+  "pat-match success without variables")
+
+(defparameter *occurs-check* t)
+
+(defun variable-p (x)
+  (and (symbolp x) (equal (char (symbol-name x) 0) #\?)))
+
+(defun get-binding (var bindings)
+  (assoc var bindings))
+
+(defun binding-val (binding)
+  (cdr binding))
+
+(defun lookup (var bindings)
+  (binding-val (get-binding var bindings)))
+
+(defun extend-bindings (var val bindings)
+  (cons (cons var val)
+        (if (and (eq bindings no-bindings))
+            nil
+            bindings)))
+
+(defun match-variable (var input bindings)
+  (let ((binding (get-binding var bindings)))
+    (cond ((not binding) (extend-bindings var input bindings))
+          ((equal input (binding-val binding)) bindings)
+          (t fail))))
+
+(defun occurs-check (var x bindings)
+  (cond ((eq var x) t)
+        ((and (variable-p x) (get-binding x bindings))
+         (occurs-check var (lookup x bindings) bindings))
+        ((consp x) (or (occurs-check var (first x) bindings)
+                       (occurs-check var (rest x) bindings)))
+        (t nil)))
+
+(defun unify-variable (var x bindings)
+  (cond ((get-binding var bindings)
+         (unify (lookup var bindings) x bindings))
+        ((and (variable-p x) (get-binding x bindings))
+         (unify var (lookup x bindings) bindings))
+        ((and *occurs-check* (occurs-check var x bindings))
+         fail)
+        (t (extend-bindings var x bindings))))
+
+(defun unify (x y &optional (bindings no-bindings))
+  (cond ((eq bindings fail) fail)
+        ((eql x y) bindings)
+        ((variable-p x) (unify-variable x y bindings))
+        ((variable-p y) (unify-variable y x bindings))
+        ((and (consp x) (consp y))
+         (unify (rest x) (rest y)
+                (unify (first x) (first y) bindings)))
+        (t fail)))
+
+(defun reuse-cons (x y x-y)
+  (if (and (eql x (car x-y)) (eql y (cdr x-y)))
+      x-y
+      (cons x y)))
+
+(defun subst-bindings (bindings x)
+  (cond ((eq bindings fail) fail)
+        ((eq bindings no-bindings) x)
+        ((and (variable-p x) (get-binding x bindings))
+         (subst-bindings bindings (lookup x bindings)))
+        ((atom x) x)
+        (t (reuse-cons (subst-bindings bindings (car x))
+                       (subst-bindings bindings (cdr x))
+                       x))))
+
+(defun unifier (x y)
+  (subst-bindings (unify x y) x))
+
+(defun clause-head (clause) (first clause))
+(defun clause-body (clause) (rest clause))
+
+(defun get-clauses (pred) (get pred 'clauses))
+(defun predicate (relation) (first relation))
+
+(defvar *db-predicates* nil)
+
+(defmacro <- (&rest clause)
+  `(add-clause ',clause))
+
+(defun add-clause (clause)
+  (let ((pred (predicate (clause-head clause))))
+    (assert (and (symbolp pred) (not (variable-p pred))))
+    (pushnew pred *db-predicates*)
+    (setf (get pred 'clauses)
+          (nconc (get-clauses pred) (list clause)))
+    pred))
+
+(defun clear-predicate (predicate)
+  (setf (get predicate 'clauses) nil))
+
+(defun clear-db ()
+  (mapc #'clear-predicate *db-predicates*))
+
+;; p. 362
