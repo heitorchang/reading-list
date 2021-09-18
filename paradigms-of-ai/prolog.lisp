105afea98d72673a394b3fc358ae526e779460e2
Heitor Chang <heitor.chang@tempook.com>
2021-09-18 18:51:02 -0300

    zebra

--- paradigms-of-ai/prolog.lisp
+++ paradigms-of-ai/prolog.lisp
@@ -83,7 +83,7 @@
 (defvar *db-predicates* nil)
 
 (defmacro <- (&rest clause)
-  `(add-clause ',clause))
+  `(add-clause ',(replace-?-vars clause)))
 
 (defun add-clause (clause)
   (let ((pred (predicate (clause-head clause))))
@@ -99,4 +99,113 @@
 (defun clear-db ()
   (mapc #'clear-predicate *db-predicates*))
 
-;; p. 362
+(defun unique-find-anywhere-if (predicate tree
+                                &optional found-so-far)
+  (if (atom tree)
+      (if (funcall predicate tree)
+          (adjoin tree found-so-far)
+          found-so-far)
+      (unique-find-anywhere-if
+       predicate
+       (first tree)
+       (unique-find-anywhere-if predicate (rest tree)
+                                found-so-far))))
+
+(defun variables-in (exp)
+  (unique-find-anywhere-if #'variable-p exp))
+
+(defun rename-variables (x)
+  (sublis (mapcar #'(lambda (var) (cons var (gensym (string var))))
+                  (variables-in x))
+          x))
+
+(defun prove-all (goals bindings)
+  (cond ((eq bindings fail) fail)
+        ((null goals) (list bindings))
+        (t (mapcan #'(lambda (goal1-solution)
+                       (prove-all (rest goals) goal1-solution))
+                   (prove (first goals) bindings)))))
+
+(defun prove (goal bindings)
+  (mapcan #'(lambda (clause)
+              (let ((new-clause (rename-variables clause)))
+                (prove-all (clause-body new-clause)
+                           (unify goal (clause-head new-clause) bindings))))
+          (get-clauses (predicate goal))))
+
+(defmacro ?- (&rest goals)
+  `(top-level-prove ',(replace-?-vars goals)))
+
+(defun replace-?-vars (exp)
+  (cond ((eq exp '?) (gensym "?"))
+        ((atom exp) exp)
+        (t (reuse-cons (replace-?-vars (first exp))
+                       (replace-?-vars (rest exp))
+                       exp))))
+
+(defun top-level-prove (goals)
+  (show-prolog-solutions
+   (variables-in goals)
+   (prove-all goals no-bindings)))
+
+(defun show-prolog-solutions (vars solutions)
+  (if (null solutions)
+      (format t "~&NO.")
+      (mapc #'(lambda (solution) (show-prolog-vars vars solution))
+            solutions))
+  (values))
+
+(defun show-prolog-vars (vars bindings)
+  (if (null vars)
+      (format t "~&Yes")
+      (dolist (var vars)
+        (format t "~&~a = ~a" var
+                (subst-bindings bindings var))))
+  (princ ";"))
+
+#| membership, zebra puzzle
+
+To compile:
+(declaim (optimize (debug 0) (safety 0) (space 0) (speed 3)))
+(compile-file #p"/home/heitor/code/reading-list/paradigms-of-ai/prolog.lisp")
+(load #P"/home/heitor/code/reading-list/paradigms-of-ai/prolog.fasl")
+
+(<- (member ?item (?item . ?rest)))
+(<- (member ?item (?x . ?rest)) (member ?item ?rest))
+
+(<- (nextto ?x ?y ?list) (iright ?y ?x ?list))
+(<- (nextto ?x ?y ?list) (iright ?x ?y ?list))
+
+(<- (iright ?left ?right (?left ?right . ?rest)))
+(<- (iright ?left ?right (?x . ?rest))
+  (iright ?left ?right ?rest))
+
+(<- (= ?x ?x))
+
+(<- (zebra ?h ?w ?z)
+(= ?h ((house norwegian ? ? ? ?)
+       ?
+       (house ? ? ? milk ?) ? ?))
+(member (house englishman ? ? ? red) ?h)
+(member (house spaniard dog ? ? ?) ?h)
+(member (house ? ? ? coffee green) ?h)
+(member (house ukranian ? ? tea ?) ?h)
+(iright (house ? ? ? ? ivory)
+        (house ? ? ? ? green) ?h)
+(member (house ? snails winston ? ?) ?h)
+(member (house ? ? kools ? yellow) ?h)
+(nextto (house ? ? chesterfield ? ?)
+        (house ? fox ? ? ?) ?h)
+(nextto (house ? ? kools ? ?)
+        (house ? horse ? ? ?) ?h)
+(member (house ? ? lucky-strike orange-juice ?) ?h)
+(member (house japanese ? parliaments ? ?) ?h)
+(nextto (house norwegian ? ? ? ?)
+        (house ? ? ? ? blue) ?h)
+
+(member (house ?w ? ? water ?) ?h)
+(member (house ?z zebra ? ? ?) ?h))
+
+to obtain answer
+(?- (zebra ?houses ?water-drinker ?zebra-owner))
+|#
