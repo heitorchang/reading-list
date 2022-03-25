fbe1c4b6aa7d3eee000966bdff692a987e7b7b69
Heitor Chang <heitorchang@gmail.com>
2022-03-25 18:12:31 -0300

    algs4

---
+++ algorithms-sedgewick/intro/BinarySearch.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int indexOf(int[] a, int key) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whitelist = {0, 1, 2};
+        System.out.println(indexOf(whitelist, 0));
+    }
+}
